42963470bf3afa001e59e680e739cb1865776ea5
kraaaken <orangeudav@gmail.com>
2018-12-08 23:54:01 +0300

    Default variable for ansible

--- terraform/bin/destroy.sh
+++ terraform/bin/destroy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -x
+set -e
 
 export AWS_ACCESS_KEY_ID=${TF_VAR_aws_access_key}
 export AWS_SECRET_ACCESS_KEY=${TF_VAR_aws_secret_key}
--- terrampiler/bin/create.sh
+++ terrampiler/bin/create.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -x
+set -e
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${CURRENT_DIR}/..
